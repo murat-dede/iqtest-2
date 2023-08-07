598eea0bb8f452678eaaa8829b386b293dba032e
karadenizemirr <karadenizemirr@gmail.com>
2023-08-07 04:43:21 +0300

    modified:   src/services/mysql.service.ts

--- src/services/mysql.service.ts
+++ src/services/mysql.service.ts
@@ -4,13 +4,27 @@ import { Score } from "src/models/score.model"
 import { User } from "src/models/user.model"
 import { DataSource } from "typeorm"
 
+// export const AppDataSource = new DataSource({
+//     type: "mysql",
+//     host: "localhost",
+//     port: 3306,
+//     username: "administrator",
+//     password: "123456",
+//     database: "iqtest",
+//     synchronize: true,
+//     logging: true,
+//     entities: [User, Score, Product,Certificate],
+//     subscribers: [],
+//     migrations: [],
+// })
+
 export const AppDataSource = new DataSource({
     type: "mysql",
-    host: "localhost",
+    host: "pei17y9c5bpuh987.chr7pe7iynqr.eu-west-1.rds.amazonaws.com",
     port: 3306,
-    username: "administrator",
-    password: "123456",
-    database: "iqtest",
+    username: "slk3ji607p2fk68o",
+    password: "ugvh6nrrbkwrelzt",
+    database: "d115riz1c86i3so2",
     synchronize: true,
     logging: true,
     entities: [User, Score, Product,Certificate],
