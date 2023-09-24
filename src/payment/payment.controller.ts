b71db50d9242b64997ae1b458be842f4ed0270e8
murat-dede <murat1_dede@hotmail.com>
2023-09-24 16:49:51 +0300

    Shopier

--- src/payment/payment.controller.ts
+++ src/payment/payment.controller.ts
@@ -1,11 +1,12 @@
 import { Body, Controller, Get, Inject, Param, Post, Render, Req, Res, Session, UseInterceptors } from "@nestjs/common";
 import { PaymentService } from "./payment.service";
-import {FastifyReply} from 'fastify'
+import Shopier from 'shopier-api'
 import * as secureSession from '@fastify/secure-session'
 import { ScoreService } from "src/services/score.service";
 import { JwtService } from "src/auth/jwt.service";
 import { UserService } from "src/user/user.service";
 import { CacheService } from "src/services/cache.service";
+
 
 @Controller('odeme')
 export class PaymentController {
@@ -13,20 +14,20 @@ export class PaymentController {
         private paymentService: PaymentService, 
         private scoreService: ScoreService, 
         private jwtService: JwtService, 
-        private userService:UserService,
+        private userService: UserService,
         private cacheService: CacheService
         
-        ) {}
+    ) {}
 
     @Get()
     @Render('payment/')
-    async get_payment_page(@Session() session:secureSession.Session):Promise<any>{
-        try{
-            const payment_form:any = await this.cacheService.get('payment_form')
+    async get_payment_page(@Session() session: secureSession.Session): Promise<any> {
+        try {
+            const payment_form: any = await this.cacheService.get('payment_form')
             const token = session.get('token')
 
             let user_data = null
-            if (token){
+            if (token) {
                 const user_id = this.jwtService.verifyToken(token['token'])
                 user_data = await this.userService.get_user_data(user_id['id'])
             }
@@ -36,40 +37,59 @@ export class PaymentController {
                 paymentScript: payment_form?.data?.CheckoutFormData,
                 user_data: user_data || null
             }
-        }catch(err){
+        } catch (err) {
             console.log(err)
         }
 
     }
 
     @Post()
-    async create_payment_form(@Body() bodyData:any, @Res() res:FastifyReply, @Session() session:secureSession.Session){
+    async create_payment_form(@Body() bodyData: any, @Res() res: Shopier, @Session() session: secureSession.Session) {
 
         const _s = await this.cacheService.get('answers')
         const _c = await this.cacheService.get('certificate') || null
 
         let product_name = _c ? 'certificate' : (_s ? 'test' : 'default');
 
-        if (_s || _c){
+        if (_s || _c) {
             const payment_form = await this.paymentService.create_payment_form(bodyData, product_name)
-            //session.set('payment_form', payment_form?.data?.CheckoutFormData)
-           await this.cacheService.set('payment_form', payment_form, 60000)
-            res.redirect(302, '/odeme')
+            // session.set('payment_form', payment_form?.data?.CheckoutFormData)
+            await this.cacheService.set('payment_form', payment_form, 60000)
+
+            // Shopier API çağrısı yaparak ödeme işlemini başlatın
+            const shopierResponse = await this.startShopierPayment(payment_form);
+            
+            // Shopier'den gelen yanıta göre işlem yapabilirsiniz
+            if (shopierResponse.success) {
+                // Başarılı işlem
+                // İşlemlerinizi burada tamamlayabilirsiniz
+                res.redirect(302, '/odeme'); // Ödeme başarılıysa teşekkür sayfasına yönlendirin
+            } else {
+                // Ödeme işlemi başarısız
+                // Hata işleme yapabilirsiniz
+                res.redirect(500, '/hata'); // Ödeme başarısızsa hata sayfasına yönlendirin
+            }
         }
         return false
     }
 
+    async startShopierPayment(paymentData: any): Promise<any> {
+        // Shopier API'sini kullanarak ödeme işlemini başlatın
+        // paymentData, Shopier API'sine gönderilecek verileri içermelidir
+        // Shopier'den gelen yanıtı döndürün
+    }
+
     @Post('callBack')
-    async callBack(@Session() session:secureSession.Session, @Body() bodyData:any, @Res() res:FastifyReply){
-        
-        if (bodyData.status === 'success'){
+    async callBack(@Session() session: secureSession.Session, @Body() bodyData: any, @Res() res: Shopier) {
+
+        if (bodyData.status === 'success') {
 
             const data = await this.cacheService.get('payment_form')
             const _s = await this.cacheService.get('answers')
             const _c = await this.cacheService.get('certificate') || null
 
             const score = _s ? await this.scoreService.score_calculate(_s['answer_json_list']) : 0;
-            //const product_name = _s && _s['product_name'] ? 'test' : 'certificate';
+            // const product_name = _s && _s['product_name'] ? 'test' : 'certificate';
             let product_name = _c ? 'certificate' : (_s ? 'test' : 'default');
 
             const save_data = {
@@ -97,11 +117,11 @@ export class PaymentController {
             session.delete()
 
             res.redirect(302, '/user/result/' + response)
-        }else{
+        } else {
             await this.cacheService.delete('payment_form')
             session.delete()
             res.redirect(500, '/')
         }
     }
-    
+
 }
